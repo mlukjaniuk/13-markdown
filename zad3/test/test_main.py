7cf8d4857c0bac17ed65c49867617ba21f068436
mlukjaniuk <ma.lukjaniuk@gmail.com>
2022-01-23 17:33:08 +0100

    .

--- zad3/test/test_main.py
+++ zad3/test/test_main.py
@@ -3,6 +3,6 @@ sys.path.append('./main')
 
 
 def test_dodaj():
-    assert main.dodaj(3, 5) == 8
-    assert main.dodaj(100, 111) == 211
-    assert main.dodaj(2.2, 3) == 5.2
+    assert dodaj(3, 5) == 8
+    assert dodaj(100, 111) == 211
+    assert dodaj(2.2, 3) == 5.2
